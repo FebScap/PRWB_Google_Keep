b58e44248b885acd874e7c0a018b85f724c900b2
NICOLAS VANHORLE <vanhorlenicolas@gmail.com>
2024-01-22 12:18:03 +0100

    feat(main) : Création des controlleurs principaux (Main et view notes)

--- controller/ControllerViewNotes.php
+++ controller/ControllerViewNotes.php
@@ -1,8 +1,8 @@
 <?php
 require_once "framework/Controller.php";
 
-class ControllerTest extends Controller {
+class ControllerViewNotes extends Controller {
     public function index() : void {
-        echo "<h1>Hello !</h1>";
+        echo "<h1>My Notes</h1>";
     }
 }
