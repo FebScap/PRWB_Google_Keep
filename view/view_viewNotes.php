b6ff846cf5f53f6e11f17d882aa7f2332ea25c2b
NICOLAS VANHORLE <vanhorlenicolas@gmail.com>
2024-01-23 19:48:56 +0100

    feat(view_notes): fin de la mise en place du visuel

--- view/view_viewNotes.php
+++ view/view_viewNotes.php
@@ -8,38 +8,36 @@
         <div class="container-fluid bg-dark text-white d-flex flex-column">
             <?php include('menu.html'); ?>
             <div class="d-flex justify-content-between mt-2">
-                <button class="btn btn-outline-light d-flex" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasMenu" aria-controls="offcanvasMenu">
+                <button class="btn btn-outline-light d-flex ms-2" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasMenu" aria-controls="offcanvasMenu">
                     <i class="bi bi-list"></i>
                 </button>
-                <h1 class="fs-4 d-flex">My notes</h1>
+                <h1 class="fs-4 d-flex me-3">My notes</h1>
             </div>
             <!-- CARDS -->
-            <div class="d-flex flex-column m-3">
-                <div class="card w-50" data-bs-theme="dark">
+            <h2 class="h2 fs-6 mt-4 ms-2">Pinned</h2>
+            <div class="d-flex flex-row flex-wrap justify-content-evenly align-items-start">
+                <div class="card m-1" style="max-width: 48%;" data-bs-theme="dark">
                     <ul class="list-group list-group-flush">
                         <li class="list-group-item">Titre</li>
                         <li class="list-group-item list-group-item-secondary">Contenu contenu contenu contenu</li>
                         <li class="list-group-item"><<         >></li>
                     </ul>
                 </div>
-
-                <div class="card w-50" data-bs-theme="dark">
+                <div class="card m-1" style="max-width: 48%;" data-bs-theme="dark">
                     <ul class="list-group list-group-flush">
                         <li class="list-group-item">Titre</li>
                         <li class="list-group-item list-group-item-secondary">Contenu contenu contenu contenu</li>
                         <li class="list-group-item"><<         >></li>
                     </ul>
                 </div>
-
-                <div class="card w-50" data-bs-theme="dark">
+                <div class="card m-1" style="max-width: 48%;" data-bs-theme="dark">
                     <ul class="list-group list-group-flush">
                         <li class="list-group-item">Titre</li>
                         <li class="list-group-item list-group-item-secondary">Contenu contenu contenu contenu</li>
                         <li class="list-group-item"><<         >></li>
                     </ul>
                 </div>
-
-                <div class="card w-50" data-bs-theme="dark">
+                <div class="card m-1" style="max-width: 48%;" data-bs-theme="dark">
                     <ul class="list-group list-group-flush">
                         <li class="list-group-item">Titre</li>
                         <li class="list-group-item list-group-item-secondary">Contenu contenu contenu contenu</li>
@@ -47,6 +45,72 @@
                     </ul>
                 </div>
             </div>
+            <h2 class="h2 fs-6 mt-1 ms-2">Other</h2>
+            <div class="d-flex flex-row flex-wrap justify-content-evenly">
+            <div class="card m-1" style="max-width: 48%;" data-bs-theme="dark">
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Titre</li>
+                        <li class="list-group-item list-group-item-secondary">Contenu contenu contenu contenu</li>
+                        <li class="list-group-item"><<         >></li>
+                    </ul>
+                </div>
+                <div class="card m-1" style="max-width: 48%;" data-bs-theme="dark">
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Titre</li>
+                        <li class="list-group-item list-group-item-secondary">Contenu contenu contenu contenu</li>
+                        <li class="list-group-item"><<         >></li>
+                    </ul>
+                </div>
+                <div class="card m-1" style="max-width: 48%;" data-bs-theme="dark">
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Titre</li>
+                        <li class="list-group-item list-group-item-secondary">Contenu contenu contenu contenu</li>
+                        <li class="list-group-item"><<         >></li>
+                    </ul>
+                </div>
+                <div class="card m-1" style="max-width: 48%;" data-bs-theme="dark">
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Titre</li>
+                        <li class="list-group-item list-group-item-secondary">Contenu contenu contenu contenu</li>
+                        <li class="list-group-item"><<         >></li>
+                    </ul>
+                </div>
+                <div class="card m-1" style="max-width: 48%;" data-bs-theme="dark">
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Titre</li>
+                        <li class="list-group-item list-group-item-secondary">Contenu contenu contenu contenu</li>
+                        <li class="list-group-item"><<         >></li>
+                    </ul>
+                </div>
+                <div class="card m-1" style="max-width: 48%;" data-bs-theme="dark">
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Titre</li>
+                        <li class="list-group-item list-group-item-secondary">Contenu contenu contenu contenu</li>
+                        <li class="list-group-item"><<         >></li>
+                    </ul>
+                </div>
+                <div class="card m-1" style="max-width: 48%;" data-bs-theme="dark">
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Titre</li>
+                        <li class="list-group-item list-group-item-secondary">Contenu contenu contenu contenu</li>
+                        <li class="list-group-item"><<         >></li>
+                    </ul>
+                </div>
+                <div class="card m-1" style="max-width: 48%;" data-bs-theme="dark">
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Titre</li>
+                        <li class="list-group-item list-group-item-secondary">Contenu contenu contenu contenu</li>
+                        <li class="list-group-item"><<         >></li>
+                    </ul>
+                </div>
+            </div>
+            <!-- BUTTONS BAS DE PAGE -->
+            <nav class="navbar fixed-bottom bg-transparent">
+                <div class="container-fluid d-flex justify-content-end">
+                    <a class="nav-link me-4 fs-2" href="#"><i class="bi bi-file-earmark text-warning"></i></a>
+                    <a class="nav-link me-4 fs-2" href="#"><i class="bi bi-card-checklist text-warning"></i></a>
+                </div>
+            </nav>
         </div>
         <?php include('footer.html'); ?>
     </body>
