039b72e881c7bfbcc0f6cd774a68faffd8756c58
NICOLAS VANHORLE <vanhorlenicolas@gmail.com>
2024-01-23 13:59:31 +0100

    feat(head & footer): creation d'un head et d'un footer commun pour les stylesheet et les scripts

--- view/view_viewNotes.php
+++ view/view_viewNotes.php
@@ -1,12 +1,8 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="UTF-8">
         <title>My notes</title>
-        <base href="<?= $web_root ?>"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="css/styles.css" rel="stylesheet" type="text/css"/>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <?php include('head.html'); ?>
     </head>
     <body>
         <?php include('menu.html'); ?>
@@ -20,7 +16,6 @@
                 </div>
             </div>
         </div>
-
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <?php include('footer.html'); ?>
     </body>
 </html>
