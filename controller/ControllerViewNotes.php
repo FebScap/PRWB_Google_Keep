a3ed24123040d0ee5e4e7e31ec008153f0488a15
CHI_TAI LE <lctai2000@hotmail.com>
2024-01-23 11:37:42 +0100

    feat(openNote & openChecklist) debut implémentation des views

--- controller/ControllerViewNotes.php
+++ controller/ControllerViewNotes.php
@@ -2,8 +2,7 @@
 require_once "framework/Controller.php";
 
 class ControllerViewNotes extends Controller {
-    //accueil du controlleur.
-    //gère l'affichage des messages et le post
+    
     public function index() : void {
         (new View("viewnotes"))->show();
     }
