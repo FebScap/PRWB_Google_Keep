39a5f9b7d03db9248e335d8bb59aa9893388aa1a
CHI_TAI LE <lctai2000@hotmail.com>
2024-01-23 16:04:01 +0100

    feat(openNote & openChelist)suite de l'implémentation des views

---
+++ controller/ControllerOpenNote.php
@@ -0,0 +1,10 @@
+<?php
+require_once "framework/Controller.php";
+
+class ControllerViewNotes extends Controller {
+    //accueil du controlleur.
+    //gère l'affichage des messages et le post
+    public function index() : void {
+        (new View("opennote"))->show();
+    }
+}
---
+++ view/view_open_checklist.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Checklist</title>
+        <base href="<?= $web_root ?>"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="form-group">
+            <label for="exampleInputEmail1">Title</label>
+            <input type="title" class="form-control" id="exampleInputTitle" aria-describedby="emailHelp" placeholder="Enter title">
+        </div>
+        <div class="title"><h1>items</h1></div>
+        <div class="main">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+        <ul class="list-group">
+            <li class="list-group-item">
+              <input class="form-check-input me-1" type="checkbox" value="" aria-label="...">
+              First checkbox
+            </li>
+            <li class="list-group-item">
+              <input class="form-check-input me-1" type="checkbox" value="" aria-label="...">
+              Second checkbox
+            </li>
+            <li class="list-group-item">
+              <input class="form-check-input me-1" type="checkbox" value="" aria-label="...">
+              Third checkbox
+            </li>
+            <li class="list-group-item">
+              <input class="form-check-input me-1" type="checkbox" value="" aria-label="...">
+              Fourth checkbox
+            </li>
+            <li class="list-group-item">
+              <input class="form-check-input me-1" type="checkbox" value="" aria-label="...">
+              Fifth checkbox
+            </li>
+          </ul>
+    </body>
+</html>
+
