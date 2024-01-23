66d296a33cbd618206f96086b371eae9d2294d0a
NICOLAS VANHORLE <vanhorlenicolas@gmail.com>
2024-01-23 21:44:44 +0100

    feat(addchecklistnote & addtextnote): finalisation du visuel pour addchecklistnote et addtextnote

--- view/view_addCheckListNote.php
+++ view/view_addCheckListNote.php
@@ -5,12 +5,36 @@
         <?php include('head.html'); ?>
     </head>
     <body data-bs-theme="dark">
-        <div class="container-fluid d-flex flex-column">
-            <?php include('menu.html'); ?>
-            <div class="d-flex justify-content-between mt-2">
-            <a class="nav-link me-4 fs-2" href="viewnotes"><i class="bi bi-box-arrow-left"></i></a>
-            <a class="nav-link me-4 fs-4" href="opentextnote"><i class="bi bi-floppy"></i></i></a>
+        <form class="container-fluid d-flex flex-column">
+            <div class="container-fluid d-flex justify-content-between">
+                <a class="nav-link me-4 fs-2" href="viewnotes"><i class="bi bi-box-arrow-left"></i></a>
+                <button type="submit" class="btn"><i class="bi bi-floppy"></i></button>
             </div>
+            <div class="mt-3">
+                <label for="noteTitle" class="form-label">Title</label>
+                <input type="text" class="form-control" id="noteTitle" aria-describedby="emailHelp">
+            </div>
+            <div class="mt-3">
+                <label class="form-label">Items</label>
+                <ul>
+                    <li class="mb-2">
+                        <input type="text" class="form-control" id="checklist1">
+                    </li>
+                    <li class="mb-2">
+                        <input type="text" class="form-control" id="checklist2">
+                    </li>
+                    <li class="mb-2">
+                        <input type="text" class="form-control" id="checklist3">
+                    </li>
+                    <li class="mb-2">
+                        <input type="text" class="form-control" id="checklist4">
+                    </li>
+                    <li class="mb-2">
+                        <input type="text" class="form-control" id="checklist5">
+                    </li>
+                </ul>
+            </div>
+        </form>
         <?php include('footer.html'); ?>
     </body>
 </html> 
