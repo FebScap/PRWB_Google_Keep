f1eb413aa2b680faa55df4269ef755e6caa80456
NICOLAS VANHORLE <vanhorlenicolas@gmail.com>
2024-01-24 15:48:42 +0100

    feat(login & signup): creation des vues et set de la page index

---
+++ view/view_login.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My notes - Login</title>
+        <?php include('head.html'); ?>
+    </head>
+    <body data-bs-theme="dark" class="h-full">
+        <div class="container h-100">
+            <div class="row align-items-center h-full">
+                <div class="col-12">
+                    <div class="card h-100 justify-content-center" style="border-radius: 1rem;">
+                    <div class="card-body p-3 text-center">
+                    <h3 class="mb-2">Sign in</h3>
+
+                    <hr class="my-4">
+
+                    <div class="input-group flex-nowrap mt-4 w-100">
+                        <button class="btn btn-outline-secondary text-white" type="button"><i class="bi bi-person"></i></button>
+                        <input type="email" class="form-control" placeholder="exemple@domaine.ect" aria-describedby="basic-addon1">
+                    </div>
+
+                    <div class="input-group flex-nowrap mt-4 w-100">
+                        <button class="btn btn-outline-secondary text-white" type="button"><i class="bi bi-key"></i></button>
+                        <input type="password" class="form-control" placeholder="Password" aria-describedby="basic-addon1">
+                    </div>
+
+                    <button class="btn btn-primary btn-lg btn-block mt-4 mb-4 w-100" type="submit">Login</button>
+                    <span class="w-100"><a class="link-underline-primary" href="signup">New here ? Click here to subscribe !</a></span>
+                </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php include('footer.html'); ?>
+    </body>
+</html> 
