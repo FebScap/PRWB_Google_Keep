db9236cba1da476448eee0921cdbd522eefcb8df
CHI_TAI LE <lctai2000@hotmail.com>
2024-01-25 19:10:03 +0100

    feat(openchecklistnote):suite de la view

--- controller/ControllerOpenNote.php
+++ controller/ControllerOpenNote.php
@@ -4,6 +4,6 @@ require_once "framework/Controller.php";
 class ControllerOpenNote extends Controller {
     //accueil du controlleur.
     public function index() : void {
-        (new View("opennote"))->show();
+        (new View("openchecklistnote"))->show();
     }
 }
--- view/view_openChecklistNote.php
+++ view/view_openChecklistNote.php
@@ -2,37 +2,29 @@
 <html>
     <head>
         <title>Checklist</title>
-        <?php include('head.html'); ?>
+        <?php include('head.html');
+        include 'view_openNote.php';
+         ?>
     </head>
     <body>
+        <p>Created 1 month ago...</p>
         <div class="form-group">
-            <label for="exampleInputEmail1">Title</label>
+            <label for="title" class ="fw-bold">Title</label>
             <input type="title" class="form-control" id="exampleInputTitle" aria-describedby="emailHelp" placeholder="Enter title">
         </div>
-        <div class="title"><h1>items</h1></div>
+        <div class="fw-bold" >Items</div>
         <div class="main">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
         <ul class="list-group">
             <li class="list-group-item">
-              <input class="form-check-input me-1" type="checkbox" value="" aria-label="...">
-              First checkbox
+              <div><input class="form-check-input" type="checkbox" id="checkboxNoLabel" value="" aria-label="...">
+
+              </div>
             </li>
             <li class="list-group-item">
               <input class="form-check-input me-1" type="checkbox" value="" aria-label="...">
               Second checkbox
             </li>
-            <li class="list-group-item">
-              <input class="form-check-input me-1" type="checkbox" value="" aria-label="...">
-              Third checkbox
-            </li>
-            <li class="list-group-item">
-              <input class="form-check-input me-1" type="checkbox" value="" aria-label="...">
-              Fourth checkbox
-            </li>
-            <li class="list-group-item">
-              <input class="form-check-input me-1" type="checkbox" value="" aria-label="...">
-              Fifth checkbox
-            </li>
+            
           </ul>
           <?php include('footer.html'); ?>
     </body>
