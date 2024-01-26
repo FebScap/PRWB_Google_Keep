4d9c20ad6db88d763b2d111413f07295ecf93ca4
NICOLAS VANHORLE <vanhorlenicolas@gmail.com>
2024-01-26 18:29:41 +0100

    fix(viewsharednotes): changement du display pour que tout les div s'affichent bien en colones

--- view/view_viewSharedNotes.php
+++ view/view_viewSharedNotes.php
@@ -14,28 +14,30 @@
                 <h1 class="fs-4 d-flex me-3">Shared by <?= $from->full_name ?></h1>
             </div>
             <!-- CARDS -->
-            <h2 class="h2 fs-6 mt-4 ms-2">Notes shared to you by <?= $from->full_name ?> as editor</h2>
+            <h2 class="d-flex h2 fs-6 mt-4 ms-2">Notes shared to you by <?= $from->full_name ?> as editor</h2>
             <div class="d-flex flex-row flex-wrap justify-content-start">
                 <?php for ($i = 0; $i < sizeof($notes); $i++): ?>
-                    <div class="card m-1" style="max-width: 48%;" data-bs-theme="dark">
-                        <a class="link-underline link-underline-opacity-0" href="opennote/">
+                    <a class="link-underline link-underline-opacity-0" style="max-width: 48%;" href="opennote/">
+                        <div class="card m-1" data-bs-theme="dark">
                             <ul class="list-group list-group-flush">
                                 <li class="list-group-item"><?= $notes[$i]->title ?></li>
                                 <li class="list-group-item list-group-item-secondary">Contenu contenu contenu contenu</li>
                             </ul>
-                        </a>
-                    </div>
+                        </div>
+                    </a>
                 <?php endfor; ?>
-                <h2 class="h2 fs-6 mt-1 ms-2">Notes shared to you by <?= $from->full_name ?> as reader</h2>
+            </div>
+            <h2 class="d-flex h2 fs-6 mt-1 ms-2">Notes shared to you by <?= $from->full_name ?> as reader</h2>
+            <div class="d-flex flex-row flex-wrap justify-content-start">
                 <?php for ($i = 0; $i < sizeof($notes); $i++): ?>
-                    <div class="card m-1" style="max-width: 48%;" data-bs-theme="dark">
-                        <a class="link-underline link-underline-opacity-0" href="opennote/">
+                    <a class="link-underline link-underline-opacity-0" style="max-width: 48%;" href="opennote/">
+                        <div class="card m-1" data-bs-theme="dark">
                             <ul class="list-group list-group-flush">
                                 <li class="list-group-item"><?= $notes[$i]->title ?></li>
                                 <li class="list-group-item list-group-item-secondary">Contenu contenu contenu contenu</li>
                             </ul>
-                        </a>
-                    </div>
+                        </div>
+                    </a>
                 <?php endfor; ?>
             </div>
         <?php include('footer.html'); ?>
