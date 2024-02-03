9765c2038160cfac3a4fe8a0db130cad83c64ad1
MartinWester <martin.driss.wester@gmail.com>
2024-02-03 16:49:32 +0100

    feat(Error): Style de la page erreur

--- view/view_error.php
+++ view/view_error.php
@@ -3,14 +3,28 @@
 
 <head>
     <title>Error</title>
-    <?php include('head.html'); ?>
     <base href="<?= $web_root ?>"/>
+    <?php include('head.html'); ?>
 </head>
 
-<body>
-    <div class="title">Error</div>
-    <div class="main">
-        <?= $error ?>
+<body data-bs-theme="dark" class="h-full">
+    <div class="container h-100">
+        <div class="row align-items-center h-full">
+            <div class="col-12">
+                <div class="card h-100 justify-content-center" style="border-radius: 1rem;">
+                    <div class="card-body p-3 text-center">
+                        <h3 class="mb-2 text-danger">Error</h3>
+
+                        <hr class="my-4">
+
+                        <div class="main text-danger">
+                            <?= $error ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+    <?php include('footer.html'); ?>
 </body>
 </html>
