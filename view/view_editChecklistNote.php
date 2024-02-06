ad479400f44e5ec2c54d7ed4c34adaa3c4581f0e
MartinWester <martin.driss.wester@gmail.com>
2024-02-07 00:06:52 +0100

    feat(EditChecklistNote): Début implémentation vue et controlleur

---
+++ view/view_editChecklistNote.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My notes - Editer une ChecklistNote</title>
+        <?php include('head.html'); ?>
+    </head>
+    <body data-bs-theme="dark">
+        <form class="container-fluid d-flex flex-column" method="post" action="OpenNote/saveChecklistNote">
+            <div class="container-fluid d-flex justify-content-between">
+                <a class="nav-link me-4 fs-2" href="opennote/index/<?= $textnote->getId() ?>"><i class="bi bi-chevron-left"></i></a>
+                <button type="submit" class="btn"><i class="bi bi-dash-square-fill icon-red"></i></button>
+            </div>
+            <div class="mt-3">
+                <label for="noteTitle" class="form-label">Title</label>
+                <input type="text" class="form-control" id="title" name="title" value="<?= $title ?>">
+                <?php if (count($errorsTitle) != 0) : ?>
+                    <label for="noteTitle" class="form-label">
+                        <?php foreach ($errorsTitle as $error): ?>
+                            <li><?= $error ?></li>
+                        <?php endforeach; ?></label>
+                <?php endif ?>
+            </div>
+            <div class="mt-3">
+                <label class="form-label">Items</label>
+                <ul>
+                    <?php for ($i=0; $i<6; $i++) {
+                        echo"
+                        <li class='mb-2'>
+                            <input type='text' class='form-control' id='checklist' name='content[$i]' value='" . $content[$i] . "'>
+                        </li>";
+                    }
+                    ?>
+                    <?php if (count($errorsContent) != 0) : ?>
+                        <label for="noteTitle" class="form-label">
+                            <?php foreach ($errorsContent as $error): ?>
+                                <li><?= $error ?></li>
+                            <?php endforeach; ?>
+                        </label>
+                    <?php endif ?>
+                </ul>
+            </div>
+        </form>
+        <?php include('footer.html'); ?>
+    </body>
+</html>
